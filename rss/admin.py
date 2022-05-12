da69aed1d50fb4e69879057a359e8febc2f6298d
CamClrt <camille.clarret@gmail.com>
2022-05-12 06:26:46 +0200

    imp feed admin

--- rss/admin.py
+++ rss/admin.py
@@ -3,7 +3,26 @@ from django.contrib import admin
 from .models import Feed, FeedContent, Tag
 
 
-admin.site.register(Feed)
-admin.site.register(FeedContent)
+# TODO: take a look at https://pypi.org/project/django-reorder-admin/ to reorder models in Django admin
 
-admin.site.register(Tag)
+
+class FeedContentInline(admin.StackedInline):
+    model = FeedContent
+
+
+class FeedAdmin(admin.ModelAdmin):
+    model = Feed
+    inlines = [FeedContentInline]
+
+
+class FeedContentAdmin(admin.ModelAdmin):
+    model = FeedContent
+
+
+class TagAdmin(admin.ModelAdmin):
+    model = Tag
+
+
+admin.site.register(Feed, FeedAdmin)
+admin.site.register(FeedContent, FeedContentAdmin)
+admin.site.register(Tag, TagAdmin)
